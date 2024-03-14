e7313eb8f05b234cb170949f5edb5cb39edd63ae
mzunohkaru <winkaisyu@gmail.com>
2024-03-14 13:50:35 +0900

    add:jwt

--- fast_api/auth/password_hash.py
+++ fast_api/auth/password_hash.py
@@ -1,4 +1,5 @@
 import bcrypt
+import secrets
 
 # 平文のパスワード
 password = "tim1234".encode('utf-8')
@@ -10,3 +11,8 @@ hashed = bcrypt.hashpw(password, bcrypt.gensalt())
 print(hashed.decode('utf-8'))
 
 # 実行結果 : $2b$12$hIr46Ha2UGYPBGQSwHDGQewiSA8I24Z4Y1d3lnR.O1pkK2afJ4z1G
+
+
+# 32バイトのランダムな文字列を生成
+secret_key = secrets.token_hex(32)
+print(secret_key)
